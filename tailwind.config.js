8ae9715c83c952894387f589aa2456cb9e4eb0a2
Lamiaa Hassan <ul24hogu@studserv.uni-leipzig.de>
2024-10-25 15:18:34 +0200

    Fix paths for assets and resolve local issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-    '/Users/lh/Documents/Projects/mat_website/index.html',
-    '/Users/lh/Documents/Projects/mat_website/src/**/*.{js,ts,jsx,tsx}', // This allows Tailwind to scan all JS and TS files for class names
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // This allows Tailwind to scan all JS and TS files for class names
   ],
   theme: {
     extend: {fontFamily: {
